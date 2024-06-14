348f7e417bb77632499dfa7d9fc13a4d0360ceb6
Moosayed1 <mohamed1212213183@gmail.com>
2024-06-14 08:44:44 +0300

    Update

--- Polygon/Check.cpp
+++ Polygon/Check.cpp
@@ -1,32 +1,39 @@
-/*
-██╗  ██╗ █████╗ ██████╗ ███████╗███████╗███╗   ███╗        ██╗  ██╗██╗  ██╗ █████╗ ██╗     ███████╗██████╗
-██║ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗ ████║        ██║ ██╔╝██║  ██║██╔══██╗██║     ██╔════╝██╔══██╗
-█████╔╝ ███████║██████╔╝█████╗  █████╗  ██╔████╔██║        █████╔╝ ███████║███████║██║     █████╗  ██║  ██║
-██╔═██╗ ██╔══██║██╔══██╗██╔══╝  ██╔══╝  ██║╚██╔╝██║        ██╔═██╗ ██╔══██║██╔══██║██║     ██╔══╝  ██║  ██║
-██║  ██╗██║  ██║██║  ██║███████╗███████╗██║ ╚═╝ ██║███████╗██║  ██╗██║  ██║██║  ██║███████╗███████╗██████╔╝
-╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═════╝
-Believe in yourself ,( try and try and then try ), You can do it
-*/
 #define _CRT_SECURE_NO_WARNINGS
-#include "testlib.h"
 #include <bits/stdc++.h>
-#include <unordered_map>
-#define endl "\n"
-#define all(v) v.begin(),v.end()
-#define rt(s) return cout<<s,0
-#define sz(s)	(int)(s.size())
-#define OO 0x3f3f3f3f3f3f3f3fLL
-#define oo 0x3f3f3f3f
+
+#include "testlib.h"
 using namespace std;
-typedef long long ll;
-int dx[] { -1, 0, 0, 1, 1, 1, -1, -1 };
-int dy[] { 0, -1, 1, 0, 1, -1, 1, -1 };
+#ifdef MOHAMED
+#include "debug.hpp"
+#else
+#define debug(...) 0
+#define debug_itr(...) 0
+#define debug_bits(...) 0
+#endif
+#define fastio()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(NULL);
+
+#define ll long long
+#define all(a) (a).begin(), (a).end()
+#define sz(a) (int)(a).size()
+#define pb push_back
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define vll vector<ll>
+#define vi vector<int>
+#define OO 1e4
+#define endl "\n"
+
+int dx[]{-1, 0, 0, 1, 1, 1, -1, -1};
+int dy[]{0, -1, 1, 0, 1, -1, 1, -1};
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
 int main(int argc, char *argv[]) {
-	registerTestlibCmd(argc, argv);
-	// inf - for the input
-	// ouf - for the contestant output
-	// ans - for the solution output
-	// quitf(_ok, "The answer is correct.");
-	// quitf(_wa, "The answer is wrong: expected = %d, found = %d", jans, pans);
-	// ensuref(bool condition, const char* message)
+  registerTestlibCmd(argc, argv);
+  // inf - for the input
+  // ouf - for the contestant output
+  // ans - for the solution output
+  // quitf(_ok, "The answer is correct.");
+  // quitf(_wa, "The answer is wrong: expected = %d, found = %d", jans, pans);
+  // ensuref(bool condition, const char* message)
 }
